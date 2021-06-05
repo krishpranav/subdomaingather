523f33388c30c833a964e81e3bfbaf5209c3ad4c
krishpranav <krisna.pranav@gmail.com>
2021-06-05 12:09:02 +0530

    [+] sub error update [+]

--- src/error.rs
+++ src/error.rs
@@ -2,3 +2,17 @@ use std::error::Error;
 use std::fmt::{self, Formatter};
 
 pub type Result<T> = std::result::Result<T, SubError>;
+
+#[derive(Debug)]
+pub enum SubError {
+    SourceError(String),
+    AuthError(String),
+    UnsetKeys(Sub<String>),
+    ReqwestError(reqwest::Error),
+    JoinError(tokio::task::JoinError),
+    IoError(std::io::Error),
+    Msg(String),
+    ParseError,
+    CrobatError,
+    EmptyResults,
+}
