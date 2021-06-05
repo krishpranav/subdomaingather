6aa6c9e39b54f262ec133fd16951fc6ac7cd0371
krishpranav <krisna.pranav@gmail.com>
2021-06-05 15:35:10 +0530

    [+] binary use [+]

---
+++ src/sources/binaryedge.rs
@@ -0,0 +1,11 @@
+use crate::error::Result;
+use crate::error::SubError;
+use crate::{DataSource, IntoSubdomain};
+use async_trait::async_trait;
+use dotenv::dotenv;
+use reqwest::Client;
+use serde::Deserialize;
+use std::env;
+use std::sync::Arc;
+use tokio::sync::mpsc::Sender;
+use tracing::{info, trace, warn};
