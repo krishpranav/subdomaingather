ffdedc0773e85b2e0c9e276781b1d6348755e13c
krishpranav <krisna.pranav@gmail.com>
2021-06-05 12:59:38 +0530

    [+] macro export [+]

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,21 @@ pub (crate) const QUEUE_SIZE: usize = 1024;
 trait DataSource: Send + Sync {
     async fn run(&self, host: Arc<String>, mut tx: mpsc::Sender<Vec<String>>) -> Result<()>;
 }
+
+#[macro_export]
+macro_rules! client {
+    ($timeout:expr, $ptimeout:expr) => {
+        reqwest::ClientBuilder::new()
+            .timeout(std::time::Duration::from_secs($timeout))
+            .pool_idle_timeout(std::time::Duration::from_secs($ptimeout))
+            .build()
+            .unwrap()
+    };
+    () => {
+        reqwest::ClientBuilder::new()
+            .timeout(std::time::Duration::from_secs(20))
+            .pool_idle_timeout(std::time::Duration::from_secs(20))
+            .build()
+            .unwrap()
+    };
+}
