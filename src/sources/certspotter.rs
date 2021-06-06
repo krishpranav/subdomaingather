587f33b7559374748d8564d53fd9b06a645656fd
krishpranav <krisna.pranav@gmail.com>
2021-06-06 11:29:47 +0530

    [+] use values [+]

---
+++ src/sources/certspotter.rs
@@ -0,0 +1,8 @@
+use crate::error::{Result, SubError};
+use crate::{DataSource, IntoSubdomain};
+use async_trait::async_trait;
+use reqwest::Client;
+use serde::Deserialize;
+use std::sync::Arc;
+use tokio::sync::mpsc::Sender;
+use tracing::{info, trace, warn};
