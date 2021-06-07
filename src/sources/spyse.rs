b1aafedc5337ef1dab39de021a86419fe7c3825f
krishpranav <krisna.pranav@gmail.com>
2021-06-07 10:29:41 +0530

    [+] spyse update [+]

--- src/sources/spyse.rs
+++ src/sources/spyse.rs
@@ -44,3 +44,96 @@ impl IntoSubdomain for SpyseResult {
         self.data.items.iter().map(|i| i.name.to_owned()).collect()
     }
 }
+
+#[derive(Default, Clone)]
+pub struct Spyse {
+    client: Client,
+}
+
+impl Spyse {
+    pub fn new(client: Client) -> Self {
+        Self { client }
+    }
+
+    fn build_url(&self, host: &str) -> String {
+        format!(
+            "https://api.spyse.com/v3/data/domain/subdomain?limit=100&domain={}",
+            host
+        )
+    }
+}
+
+#[async_trait]
+impl DataSource for Spyse {
+    async fn run(&self, host: Arc<String>, mut tx: Sender<Vec<String>>) -> Result<()> {
+        trace!("fetching data from spyse for: {}", &host);
+        let token = match Creds::read_creds() {
+            Ok(creds) => creds.token,
+            Err(e) => return Err(e),
+        };
+
+        let uri = self.build_url(&host);
+        let resp = self
+            .client
+            .get(&uri)
+            .header(ACCEPT, "application/json")
+            .bearer_auth(token)
+            .send()
+            .await?;
+
+        if resp.status().is_client_error() {
+            warn!("got status: {} from spyse", resp.status().as_str());
+            return Err(SubError::AuthError("Spyse".into()));
+        } else {
+            let resp: Option<SpyseResult> = resp.json().await?;
+            if let Some(data) = resp {
+                let subdomains = data.subdomains();
+                info!("Discovered {} results for {}", &subdomains.len(), &host);
+                let _ = tx.send(subdomains).await;
+                return Ok(());
+            }
+        }
+
+        warn!("no results for {} from Spyse", &host);
+        Err(SubError::SourceError("Spyse".into()))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use matches::matches;
+    use tokio::sync::mpsc::channel;
+
+    #[test]
+    fn url_builder() {
+        let correct_uri =
+            "https://api.spyse.com/v3/data/domain/subdomain?limit=100&domain=hackerone.com";
+        assert_eq!(correct_uri, Spyse::default().build_url("hackerone.com"));
+    }
+
+    // Checks to see if the run function returns subdomains
+    #[ignore]
+    #[tokio::test]
+    async fn returns_results() {
+        let (tx, mut rx) = channel(1);
+        let host = Arc::new("hackerone.com".to_owned());
+        let _ = Spyse::default().run(host, tx).await;
+        let mut results = Vec::new();
+        for r in rx.recv().await {
+            results.extend(r)
+        }
+        assert!(!results.is_empty());
+    }
+
+    #[ignore]
+    #[tokio::test]
+    async fn handle_no_results() {
+        let (tx, _rx) = channel(1);
+        let host = Arc::new("anVubmxpa2VzdGVh.com".to_string());
+        assert!(matches!(
+            Spyse::default().run(host, tx).await.err().unwrap(),
+            SubError::SourceError(_)
+        ));
+    }
+}
