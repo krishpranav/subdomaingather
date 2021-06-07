11c0aa4fd68423c1bda4702cec24cd08a2fb36c9
krishpranav <krisna.pranav@gmail.com>
2021-06-07 08:20:31 +0530

    [+] sonar search update [+]

---
+++ src/sources/sonarsearch.rs
@@ -0,0 +1,14 @@
+use crate::error::{Result, SubError};
+use crate::{DataSource, QUEUE_SIZE};
+use async_trait::async_trait;
+use crobat::Crobat;
+use futures::StreamExt;
+use reqwest::Client;
+use std::sync::Arc;
+use tokio::sync::mpsc::Sender;
+use tracing::{debug, info};
+
+#[derive(Default, Clone)]
+pub struct SonarSearch {
+    client: Client,
+}
